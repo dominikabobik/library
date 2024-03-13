c7f2701e530c96b72d4a7fa13e440e1ea45349db
Dominika Bobik <dominikabobik12@gmail.com>
2024-03-12 19:15:02 -0700

    create user works

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+use std::{
+    collections::HashMap,
+    io::{self, stdin},
+};
+
+use uuid::Uuid;
+
+use crate::user::{create_account, login, User};
+pub mod user;
+
+fn main() {
+    let mut user_list: HashMap<Uuid, User> = HashMap::new();
+
+
+    while true {
+    println!("Hello! Choose one of the following options:");
+    println!("(1) Login");
+    println!("(2) Create an account");
+    let mut input: String = String::new();
+    stdin().read_line(&mut input).unwrap();
+    if input.trim() == "1" {
+        login();
+    } else if input.trim() == "2" {
+        create_account(&mut user_list);
+        println!("Current user list:");
+        for (id, user) in &user_list {
+            println!("{id}: {0}", user.name);
+        }
+    } else {
+        println!("Incorrect input. Bye!");
+        return;
+    }
+    }
+}
---
+++ src/user.rs
@@ -0,0 +1,25 @@
+use std::{collections::HashMap, io::stdin};
+
+use uuid::Uuid;
+
+pub struct User {
+    pub id: Uuid,
+    pub name: String,
+    pub password: String,
+}
+
+pub fn login() {
+    println!("Login...");
+}
+
+pub fn create_account(user_list: &mut HashMap<Uuid, User>) {
+    println!("Create an account...");
+    println!("Enter your name:");
+    let mut name: String = String::new();
+    stdin().read_line(&mut name).unwrap();
+    println!("Enter your password:");
+    let mut password: String = String::new();
+    stdin().read_line(&mut password).unwrap();
+    let uuid: Uuid = Uuid::new_v4();
+    user_list.insert(uuid, User {id: uuid, name: name, password: password});
+}
